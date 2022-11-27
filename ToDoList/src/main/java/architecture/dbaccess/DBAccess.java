554ffa6bfe5351e1c26d5e113dc56d729b11692c
AngeloIossa <angelo.iossa.work@gmail.com>
2022-11-27 16:51:49 +0100

    add file for connection to db and SQL files

--- ToDoList/src/main/java/architecture/dbaccess/DBAccess.java
+++ ToDoList/src/main/java/architecture/dbaccess/DBAccess.java
@@ -1,5 +1,46 @@
 package architecture.dbaccess;
 
-public class DBAccess {
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
+import architecture.dao.DAOException;
+
+public class DBAccess {
+	private static Connection conn;
+	
+	public static synchronized Connection getConnection() throws ClassNotFoundException, IOException, DAOException {
+		try {
+			// Take current thread for synchronization
+			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+			// Read properties from config.properties, connection to OracleDB
+			InputStream input = classLoader.getResourceAsStream("properties/config.properties");
+			Properties fileProperties = new Properties();
+			fileProperties.load(input);
+			
+			Class.forName(fileProperties.getProperty("jdbcDriver")); // Get driver class
+			// Get connection
+			conn = DriverManager.getConnection(
+					fileProperties.getProperty("jdbcURL"),
+					fileProperties.getProperty("username"),
+					fileProperties.getProperty("password"));
+			conn.setAutoCommit(false); // No autocommit
+			return conn;		
+		} catch (SQLException sql) {
+			throw new DAOException(sql);
+		}
+	}
+	
+	public static void closeConnection() throws DAOException {
+		try {
+			if(conn != null) {
+				conn.close();
+			}
+		} catch (SQLException sql) {
+			throw new DAOException(sql);
+		}
+	}
 }
